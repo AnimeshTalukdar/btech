c0b752135f0de5f01bc8ab9f09705101c9951068
be21149 <be21149@mail.com>
2022-11-25 16:37:38 +0530

    autopush

--- year2/oop/prog21.cpp
+++ year2/oop/prog21.cpp
@@ -1,22 +1,90 @@
-#include <iostream>
-using namespace std;
-
-
-class Vector {
-public :
-int * arr;
-int size;
-
-Vector( int k)
+ class Vector
 {
-size =k;
-arr= new int [k];
-cout<<"Enter the elements of the array : " <<endl;
-for ( int i=0;i<k;i++)
-cin>>arr[i];
+int n;
+int cc;
+int *arr;
+public:
+Vector(int n=2147483647)
+{
+arr= new int [n];
+cc=0;
 }
 
-Vector addition ( Vector a,Vector b)
+int size()
 {
-Vector sum = new Vector(a.size);
-sum
+return cc;
+}
+void push(int k)
+{
+arr[cc++]=k;
+}
+
+int get(int i)
+{
+return arr[i];
+}
+
+void set(int i,int k)
+{
+arr[i]=k;
+if(i>cc)cc=i;
+}
+
+void add(Vector a,Vector b)
+{
+for(int i=0;i<b.size();i++)
+{
+a.set(i,a.get(i)+b.get(i));
+}
+}
+
+void sub(Vector a,Vector b)
+{
+for(int i=0;i<b.size();i++)
+{
+a.set(i,a.get(i)-b.get(i));
+}
+}
+
+bool equalityy(Vector a,Vector b)
+{
+bool con=true;
+for(int i=0;i<b.size();i++)
+{
+if(a.get(i)!=b.get(i))
+{
+con=false;
+}
+}
+return con&&(a.size()==b.size());
+}
+bool less(Vector a,Vector b)
+{
+int suma=0,sumb=0;
+for(int i=0;i<a.size();i++)
+{
+suma=suma+a.get(i);
+}
+for(int i=0;i<b.size();i++)
+{
+sumb=sumb+b.get(i);
+}
+return suma<sumb;
+}
+bool more(Vector a,Vector b)
+{
+int suma=0,sumb=0;
+for(int i=0;i<a.size();i++)
+{
+suma=suma+a.get(i);
+}
+for(int i=0;i<b.size();i++)
+{
+sumb=sumb+b.get(i);
+}
+return suma>sumb;                         
+}
+
+
+};
+

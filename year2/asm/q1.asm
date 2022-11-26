e1254fc1ba868982086ba6a04e664a7a0ba7e2f9
AnimeshTalukdar <96009749+AnimeshTalukdar@users.noreply.github.com>
2022-11-26 15:48:59 +0530

    Added asm files assignment 1

---
+++ year2/asm/q1.asm
@@ -0,0 +1,38 @@
+.MODEL SMALL
+
+
+        .STACK 100H
+
+
+        .DATA
+
+
+               DATA1 DB 02H
+               DATA2 DB 05H
+ 
+
+        .CODE
+
+         MAIN PROC FAR
+
+
+                MOV AX,@DATA
+
+                MOV DS,AX
+
+                MOV BL,9
+                MOV DL,12
+                SUB DL,BL
+                ADD DL,48
+                MOV AH,2
+                INT 21H
+                MOV AH,4CH
+
+		INT 21H
+
+
+
+        MAIN ENDP
+
+
+        END  MAIN

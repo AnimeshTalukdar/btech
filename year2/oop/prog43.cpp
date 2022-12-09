e314fb25b2f0cca38a86081f824c2c60c2a51c69
be21149 <be21149@mail.com>
2022-12-09 16:20:11 +0530

    did nothing today it was great

---
+++ year2/oop/prog43.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class Shape2D{
+double length, width;
+public:
+Shape2D(double l=0,double w=0)
+{
+length =l;
+width=w;
+};
+
+int main()
+{
+
+return 0;
+}
